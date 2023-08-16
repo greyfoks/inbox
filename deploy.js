17244e8ce0317c29dfbbc537d05c48f84cf0768d
cnetten <chris.netten@altrac.io>
2023-08-16 19:52:06 -0400

    update to newest version

--- deploy.js
+++ deploy.js
@@ -1,5 +1,26 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+require('dotenv').config()
 
-// deploy code will go here
+const { abi , evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+    process.env.MNEMONIC,
+    process.env.TEST_NETWORK
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account ', accounts[0]);
+
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments:['Hi there!'] })
+        .send({ from: accounts[0] });
+
+    console.log('Contract deployed to ', result.options.address);
+    provider.engine.stop()
+}
+deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,21 +3,22 @@ const { Web3 } = require('web3');
 const assert = require('assert');
 
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require('../compile');
+const { abi, evm } = require('../compile');
 // contract test code will go here
 
 let accounts;
 let inbox; 
+const INITIAL_STRING = 'Hi there!';
 
 beforeEach(async () => {
     // Get a list of all accounts
    accounts = await web3.eth.getAccounts();
    
    // Use one of those accounts to deploy the contract
-   inbox = await new web3.eth.Contract(JSON.parse(interface))
+   inbox = await new web3.eth.Contract(abi)
     .deploy({ 
-        data: bytecode, 
-        arguments: ['Hi there!'] 
+        data: evm.bytecode.object, 
+        arguments: [INITIAL_STRING] 
     })
     .send({ from: accounts[0], gas: '1000000' });
 
@@ -28,7 +29,14 @@ describe('Inbox',() => {
         assert.ok(inbox.options.address);
     });
 
-    it('Has a default message', () => {
-        
+    it('Has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING);
+    });
+
+    it('Can change the message', async () => {
+        await inbox.methods.setMessage('The new message!').send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'The new message!');
     });
 });
